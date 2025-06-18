87c51cab6fae3f5fbadd424fe92a6305bab22bcf
rinn0 <rinn0809@icloud.com>
2025-06-19 01:58:21 +0900

    OK

--- app/src/main/java/com/example/techtrain/railway/android/MainActivity.kt
+++ app/src/main/java/com/example/techtrain/railway/android/MainActivity.kt
@@ -1,14 +1,56 @@
 package com.example.techtrain.railway.android
 
 import android.os.Bundle
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.example.techtrain.railway.android.databinding.ActivityMainBinding
+import android.content.Intent
+import android.util.Log
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding  // バインディング変数
+    private val TAG = "MainActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)  // バインディング初期化
+        setContentView(binding.root)
 
-        findViewById<TextView>(R.id.text).text = "Hello Railway!"
+        binding.text.text = "Hello Railway!"
+
+        binding.button.setOnClickListener {
+            val inputText = binding.editText.text.toString()
+            binding.text.text = inputText
+
+            val intent = Intent(this, SecondActivity::class.java)
+            intent.putExtra("KEY_INPUT_TEXT", inputText)  // ここでテキストをIntentに入れる
+            startActivity(intent)
+        }
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy")
     }
 }
---
+++ app/src/main/java/com/example/techtrain/railway/android/SecondActivity.kt
@@ -0,0 +1,33 @@
+package com.example.techtrain.railway.android
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.techtrain.railway.android.databinding.ActivitySecondBinding
+
+class SecondActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySecondBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        binding = ActivitySecondBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // Intentからテキストを受け取る
+        val inputText = intent.getStringExtra("KEY_INPUT_TEXT") ?: "テキストがありません"
+
+        // TextView にセットする
+        binding.textView.text = inputText
+    }
+}
